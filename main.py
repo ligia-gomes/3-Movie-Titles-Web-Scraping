c5675cc1e2b76a31bee2aed22b7c53c43777eb0c
Lígia Gomes <ligiainesgomes@gmail.com>
2024-04-28 15:45:09 +0100

    adding files to repo

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+all_movies = soup.find_all(name="h3", class_="title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+
+with open("movies-test.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
+
+
+'''
+FAQ: Empire's website has changed!
+
+We used this URL for the project:
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+
+However, Empire has since changed their website. The h3 with the class "title" is no longer there. 
+To use the same code, we can use a cached version of the website from the Internet Archive's Wayback Machine.
+
+'''
